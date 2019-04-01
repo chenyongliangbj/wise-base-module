462350e3786cc21494890e1462fe8b63c09544b8
cyl524414140 <31237247+cyl524414140@users.noreply.github.com>
2019-04-01 10:04:16 +0800

    Initial commit

---
+++ src/main/java/com/chenyl/module/java/HashMapTest.java
@@ -0,0 +1,12 @@
+package com.chenyl.module.java;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class HashMapTest {
+    public static void main(String[] args){
+
+        HashMap hashMap;
+        ConcurrentHashMap map ;
+    }
+}
