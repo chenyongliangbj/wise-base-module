38fdb992c105a7ff7ca64a319e7eb2cbb8c124e4
陈永亮 <yongliang.chen@hanshow.com>
2019-12-19 15:07:00 +0800

    线程

--- src/main/java/com/chenyl/module/base/Thread/base/E_JoinThread2.java
+++ src/main/java/com/chenyl/module/base/Thread/base/E_JoinThread2.java
@@ -15,51 +15,39 @@ import com.chenyl.module.utils.SleepTools;
  * 你去打饭，一个美女插到前面打饭，美女打完饭你才能打饭。
  * 美女的男朋友插到前面，美女的男朋友打完饭，美女才能打饭，美女打完饭，你才能打饭。
  **/
-public class E_JoinThread {
+public class E_JoinThread2 {
 
-
-    static class Goddess implements Runnable {
+    static class Girl implements Runnable {
         private Thread thread;
 
-        public Goddess(Thread thread) {
+        public Girl(Thread thread) {
             this.thread = thread;
         }
 
-        public Goddess() {
+        public Girl() {
         }
 
         public void run() {
-            System.out.println("Goddess开始排队打饭.....");
+            System.out.println("Girl 开始排队打饭.....");
             try {
                 if(thread!=null) thread.join();
             } catch (InterruptedException e) {
             }
             SleepTools.second(2);//休眠2秒
             System.out.println(Thread.currentThread().getName()
-                    + " Goddess打饭完成.");
+                    + " Girl 打饭完成.");
         }
     }
 
-    static class GoddessBoyfriend implements Runnable {
 
-        public void run() {
-            SleepTools.second(2);//休眠2秒
-            System.out.println("GoddessBoyfriend开始排队打饭.....");
-            System.out.println(Thread.currentThread().getName()
-                    + " GoddessBoyfriend打饭完成.");
-        }
-    }
 
     public static void main(String[] args) throws InterruptedException {
-        GoddessBoyfriend goddessBoyfriend = new GoddessBoyfriend();
-        Thread gbf = new Thread(goddessBoyfriend);
-        Goddess goddess = new Goddess(gbf);
-        Thread g = new Thread(goddess);
+        Girl girl = new Girl();
+        Thread g = new Thread(girl);
         g.start();
-        gbf.start();
-        System.out.println("lison开始排队打饭.....");
+        System.out.println("小亮开始排队打饭.....");
         g.join();
         SleepTools.second(2);//让主线程休眠2秒
-        System.out.println(Thread.currentThread().getName() + " lison打饭完成.");
+        System.out.println(Thread.currentThread().getName() + " 小亮打饭完成.");
     }
 }
