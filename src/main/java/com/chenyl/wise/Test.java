90e4afbec8f0091bce6ac0322ca671eeb16fabe8
chenyongliangbj <chenyongliangbj@163.com>
2020-09-07 20:28:43 +0800

    test

--- src/main/java/com/chenyl/wise/Test.java
+++ src/main/java/com/chenyl/wise/Test.java
@@ -1,12 +1,15 @@
 package com.chenyl.wise;
 
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.XmlWebApplicationContext;
-
 public class Test {
     public static void main(String[] args) {
-        XmlWebApplicationContext webApplicationContext;
 
-        WebApplicationContext webApplicationContext1;
+        Test test = new Test();
+        test.B();
+
+
+    }
+
+    public void B(){
+        B();
     }
 }
